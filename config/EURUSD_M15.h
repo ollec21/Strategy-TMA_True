94a3aece9a24fa14947cdbb59022f6faf6f76a34
kenorb <kenorb@users.noreply.github.com>
2021-01-11 06:13:39 +0000

    Optimize SignalOpen params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -1,12 +1,18 @@
-/*
+/**
  * @file
- * Defines strategy's and indicator's default parameter values
- * for the given pair symbol and timeframe.
+ * Defines default strategy parameter values for the given timeframe.
  */
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_TMA_True_Params_M15 : Indi_TMA_True_Params {
-  Indi_TMA_True_Params_M15() : Indi_TMA_True_Params(indi_tmat_defaults, PERIOD_M15) { shift = 0; }
+  Indi_TMA_True_Params_M15() : Indi_TMA_True_Params(indi_tmat_defaults, PERIOD_M15) {
+    atr_multiplier = 0;
+    atr_period = 0;
+    atr_tf = 0;
+    bars_to_process = 0;
+    half_length = 0;
+    shift = 0;
+  }
 } indi_tmat_m15;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
@@ -14,14 +20,14 @@ struct Stg_TMA_True_Params_M15 : StgParams {
   // Struct constructor.
   Stg_TMA_True_Params_M15() : StgParams(stg_tmat_defaults) {
     lot_size = 0;
-    signal_open_method = 0;
-    signal_open_filter = 1;
-    signal_open_level = (float)0;
-    signal_open_boost = 0;
+    signal_open_method = -4;
+    signal_open_filter = 0;
+    signal_open_level = (float)0.0;
+    signal_open_boost = 1;
     signal_close_method = 0;
-    signal_close_level = (float)0;
+    signal_close_level = (float)0.0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)0.0;
     tick_filter_method = 1;
     max_spread = 0;
   }
--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -1,12 +1,18 @@
-/*
+/**
  * @file
- * Defines strategy's and indicator's default parameter values
- * for the given pair symbol and timeframe.
+ * Defines default strategy parameter values for the given timeframe.
  */
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_TMA_True_Params_M5 : Indi_TMA_True_Params {
-  Indi_TMA_True_Params_M5() : Indi_TMA_True_Params(indi_tmat_defaults, PERIOD_M5) { shift = 0; }
+  Indi_TMA_True_Params_M5() : Indi_TMA_True_Params(indi_tmat_defaults, PERIOD_M5) {
+    atr_multiplier = 0;
+    atr_period = 0;
+    atr_tf = 0;
+    bars_to_process = 0;
+    half_length = 0;
+    shift = 0;
+  }
 } indi_tmat_m5;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
@@ -15,13 +21,13 @@ struct Stg_TMA_True_Params_M5 : StgParams {
   Stg_TMA_True_Params_M5() : StgParams(stg_tmat_defaults) {
     lot_size = 0;
     signal_open_method = 0;
-    signal_open_filter = 1;
-    signal_open_level = (float)0;
-    signal_open_boost = 0;
+    signal_open_filter = 0;
+    signal_open_level = (float)0.0;
+    signal_open_boost = 1;
     signal_close_method = 0;
-    signal_close_level = (float)0;
+    signal_close_level = (float)0.0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)0.0;
     tick_filter_method = 1;
     max_spread = 0;
   }
